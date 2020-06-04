6ffcd6c98bbaa5b7176e90b8ee79162cef700e3a
qx4mx <qx4mx1@gmail.com>
2020-06-04 15:15:33 +0200

    css btn padding

--- src/app/services/baseUrl.ts
+++ src/app/services/baseUrl.ts
@@ -1,5 +1,5 @@
 //https://angular.vidsync.de
 //http://localhost:4000
 //export const baseUrl:string = "angular.vidsync.de";
-//export const baseUrl:string = "http://localhost:4000";
-export const baseUrl:string = window.origin;
+export const baseUrl:string = "http://localhost:4000";
+//export const baseUrl:string = window.origin;

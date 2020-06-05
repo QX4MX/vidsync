9fed0155cd7f6141a6f0cd2c9303499af12b3d5c
qx4mx <qx4mx1@gmail.com>
2020-06-05 16:32:33 +0200

    fix baseUrls

--- src/backend/api/dbUrl.ts
+++ src/backend/api/dbUrl.ts
@@ -1,3 +1,3 @@
 //
-export const baseUrl:string = "mongodb://localhost:27017/vidsyncdb";
-//export const baseUrl:string = "mongodb://mongodb.default.svc.cluster.local:27017/vidsyncdb";
+//export const baseUrl:string = "mongodb://localhost:27017/vidsyncdb";
+export const baseUrl:string = "mongodb://mongodb.default.svc.cluster.local:27017/vidsyncdb";

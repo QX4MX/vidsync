451a4002249c04ac57f12e32a41d527f3bdd64c7
qx4mx <qx4mx1@gmail.com>
2020-06-07 16:56:52 +0200

    format services frontend code

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,90 +2,90 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import {baseUrl} from './baseUrl';
+import { baseUrl } from './baseUrl';
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 
 export class ApiService {
-  
-  headers = new HttpHeaders().set('Content-Type', 'application/json');
 
-  constructor(private http: HttpClient) {
-    console.log("Expecting Api at " + baseUrl+"/api");
-   }
+	headers = new HttpHeaders().set('Content-Type', 'application/json');
 
-  // Create
-  createRoom(data): Observable<any> {
-    let url = `${baseUrl}/api/room`;
-    return this.http.post(url, data)
-      .pipe(
-        catchError(this.errorMgmt)
-      )
-  }
+	constructor(private http: HttpClient) {
+		console.log("Expecting Api at " + baseUrl + "/api");
+	}
 
-  // Get all users
-  getRooms() {
-    return this.http.get(`${baseUrl}/api/room`);
-  }
+	// Create
+	createRoom(data): Observable<any> {
+		let url = `${baseUrl}/api/room`;
+		return this.http.post(url, data)
+			.pipe(
+				catchError(this.errorMgmt)
+			)
+	}
 
-  // Get user
-  getRoom(id): Observable<any> {
-    let url = `${baseUrl}/api/room/${id}`;
-    return this.http.get(url, {headers: this.headers}).pipe(
-      map((res: Response) => {
-        return res || {}
-      }),
-      catchError(this.errorMgmt)
-    )
-  }
+	// Get all users
+	getRooms() {
+		return this.http.get(`${baseUrl}/api/room`);
+	}
 
-  // Update user
-  updateRoom(id,data): Observable<any> {
-    let url = `${baseUrl}/api/room/${id}`;
-    return this.http.put(url,data, {headers: this.headers}).pipe(
-      map((res: Response) => {
-        console.log("response:")
-        console.log(res);
-        return res || {}
-      }),
-      catchError(this.errorMgmt)
-    )
-  }
+	// Get user
+	getRoom(id): Observable<any> {
+		let url = `${baseUrl}/api/room/${id}`;
+		return this.http.get(url, { headers: this.headers }).pipe(
+			map((res: Response) => {
+				return res || {}
+			}),
+			catchError(this.errorMgmt)
+		)
+	}
 
-  /* deleteRoom(id): Observable<any> {
-    let url = `${baseUrl}/api/room/${id}`;
-    return this.http.delete(url, { headers: this.headers }).pipe(
-      catchError(this.errorMgmt)
-    )
-  } */
+	// Update user
+	updateRoom(id, data): Observable<any> {
+		let url = `${baseUrl}/api/room/${id}`;
+		return this.http.put(url, data, { headers: this.headers }).pipe(
+			map((res: Response) => {
+				console.log("response:")
+				console.log(res);
+				return res || {}
+			}),
+			catchError(this.errorMgmt)
+		)
+	}
 
-  // Error handling 
-  errorMgmt(error: HttpErrorResponse) {
-    let errorMessage = '';
-    if (error.error instanceof ErrorEvent) {
-      // Get client-side error
-      errorMessage = error.error.message;
-    } else {
-      // Get server-side error
-      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-    }
-    return throwError(errorMessage);
-  }
+	/* deleteRoom(id): Observable<any> {
+	  let url = `${baseUrl}/api/room/${id}`;
+	  return this.http.delete(url, { headers: this.headers }).pipe(
+		catchError(this.errorMgmt)
+	  )
+	} */
 
-  // Get all users
-  adminGetRooms(pw:string) {
-    return this.http.get(`${baseUrl}/api/admin/room`,{
-      params:{
-        pw: pw
-      }
-    });
-  }
+	// Error handling 
+	errorMgmt(error: HttpErrorResponse) {
+		let errorMessage = '';
+		if (error.error instanceof ErrorEvent) {
+			// Get client-side error
+			errorMessage = error.error.message;
+		} else {
+			// Get server-side error
+			errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+		}
+		return throwError(errorMessage);
+	}
 
-  adminDeleteRoom(id,pw): Observable<any> {
-    let url = `${baseUrl}/api/room/${id}`;
-    return this.http.delete(url, { headers: this.headers, params:{pw:pw} }).pipe(
-      catchError(this.errorMgmt)
-    )
-  }
+	// Get all users
+	adminGetRooms(pw: string) {
+		return this.http.get(`${baseUrl}/api/admin/room`, {
+			params: {
+				pw: pw
+			}
+		});
+	}
+
+	adminDeleteRoom(id, pw): Observable<any> {
+		let url = `${baseUrl}/api/room/${id}`;
+		return this.http.delete(url, { headers: this.headers, params: { pw: pw } }).pipe(
+			catchError(this.errorMgmt)
+		)
+	}
 }
--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -4,13 +4,13 @@ import { SocketEvent } from '../Enums';
 import { baseUrl } from './baseUrl';
 
 @Injectable({
-  	providedIn: 'root'
+	providedIn: 'root'
 })
 
 export class SocketService {
-  //https://angular.vidsync.de
-  //http://localhost:4000
-	public socket : SocketIOClient.Socket;
+	//https://angular.vidsync.de
+	//http://localhost:4000
+	public socket: SocketIOClient.Socket;
 	constructor() {
 		console.log("Connecting Socket at " + baseUrl);
 		this.socket = io.connect(baseUrl);

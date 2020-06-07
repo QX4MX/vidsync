852cdaeb8a41aa82e49baf83bfefdea22c372bd4
qx4mx <qx4mx1@gmail.com>
2020-06-07 16:53:10 +0200

    format model frontend code

--- src/app/model/room.spec.ts
+++ src/app/model/room.spec.ts
@@ -1,7 +1,7 @@
 import { Room } from './room';
 
 describe('Room', () => {
-  it('should create an instance', () => {
-    expect(new Room()).toBeTruthy();
-  });
+	it('should create an instance', () => {
+		expect(new Room()).toBeTruthy();
+	});
 });
--- src/app/model/room.ts
+++ src/app/model/room.ts
@@ -1,11 +1,11 @@
 export class Room {
     name: string
     privacy: string
-    permanent: string;      
-    editable: string;  
-    creator:string;         
-    video:string;
+    permanent: string;
+    editable: string;
+    creator: string;
+    video: string;
     queue: Array<string>;
-    created_date :Date
+    created_date: Date
 }
 

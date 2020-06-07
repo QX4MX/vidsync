ff1b66887f3be7502df4415e468a6d8745fc0533
qx4mx <qx4mx1@gmail.com>
2020-06-07 16:59:07 +0200

    format frontend code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,24 @@
-import { Component ,Output , EventEmitter} from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {  
-  
+export class AppComponent {
 
-  constructor() {
-	
-  }
 
-  ngOnInit(): void {
-    let tag = document.createElement('script');
+	constructor() {
+
+	}
+
+	ngOnInit(): void {
+		let tag = document.createElement('script');
 		tag.src = "https://www.youtube.com/iframe_api";
 		let firstScriptTag = document.getElementsByTagName('script')[0];
 		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  }
+	}
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {RouterModule, Routes} from '@angular/router';
-import {LayoutModule} from './layout/layout.module';
-import {RoomsModule} from './rooms/rooms.module';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutModule } from './layout/layout.module';
+import { RoomsModule } from './rooms/rooms.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AdminModule } from './admin/admin.module';
 import { UsersModule } from './users/users.module';
@@ -13,30 +13,29 @@ import { IndexModule } from './index/index.module';
 
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/',
-    pathMatch: 'full'
-  }
+	{
+		path: '',
+		redirectTo: '/',
+		pathMatch: 'full'
+	}
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    RouterModule.forRoot(routes),
-    LayoutModule,
-    IndexModule,
-    UsersModule,
-    RoomsModule,
-    AdminModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		RouterModule.forRoot(routes),
+		LayoutModule,
+		IndexModule,
+		UsersModule,
+		RoomsModule,
+		AdminModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
- 

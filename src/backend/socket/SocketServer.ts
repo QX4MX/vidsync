2dd3abecdd750ebadc6878e3f3e5507ad670367f
qx4mx <qx4mx1@gmail.com>
2020-06-16 17:20:36 +0200

    added seperate backend socketfiles

---
+++ src/backend/socket/SocketServer.ts
@@ -0,0 +1,145 @@
+import { Server } from 'http';
+import * as socketIo from 'socket.io';
+import { SocketEvent } from './SocketEvents';
+import { SocketRoom } from './SocketRoom';
+import { mongooseDB } from '../api/Database';
+import { youtubeapi } from '../ytapi';
+
+export class SocketServer {
+    private io: SocketIO.Server;
+    private currentRooms: Map<string, SocketRoom> = new Map();
+    private coolDownTime: number = 150;
+
+    constructor(server: Server, private ytApi: youtubeapi) {
+        this.io = socketIo(server);
+        this.io.on(SocketEvent.CONNECT, (socket: SocketIO.Socket) => {
+            console.log("socket connected");
+            socket.on(SocketEvent.DISCONNECT, () => {
+                this.currentRooms.forEach((room: SocketRoom, key: string) => {
+                    for (let user of room.getUsers()) {
+                        if (user == socket.id) {
+                            room.userLeave(socket.id);
+                            if (room.getUserCount() <= 0) {
+                                this.currentRooms.delete(key);
+                                console.log("cleared");
+                            }
+                            this.io.to(key).emit(SocketEvent.GETUSERS, room.getUserCount());
+                        }
+                    }
+                });
+                console.log("socket disconnected");
+            });
+            // TODO Handle roomid for user
+            socket.on(SocketEvent.JOIN, (roomId: string) => {
+                socket.join(roomId, () => {
+                    console.log("Joined room " + roomId);
+                    let room = this.currentRooms.get(roomId);
+                    if (!room) {
+                        room = new SocketRoom(roomId);
+                        room.userJoin(socket.id);
+                        this.currentRooms.set(roomId, room);
+                    }
+                    else {
+                        room.userJoin(socket.id);
+                        this.currentRooms.set(roomId, room);
+                    }
+                    this.io.to(roomId).emit(SocketEvent.GETUSERS, room.getUserCount());
+                });
+            });
+
+            socket.on(SocketEvent.LEAVE, () => {
+                this.currentRooms.forEach((room: SocketRoom, key: string) => {
+                    for (let user of room.getUsers()) {
+                        if (user == socket.id) {
+                            room.userLeave(socket.id);
+                            if (room.getUserCount() <= 0) {
+                                this.currentRooms.delete(key);
+                                console.log("cleared");
+                            }
+                            this.io.to(key).emit(SocketEvent.GETUSERS, room.getUserCount());
+                        }
+                    }
+                });
+                console.log("socket left");
+            });
+
+
+            socket.on(SocketEvent.PLAY, (roomId: string, time: number) => {
+                let room = this.currentRooms.get(roomId);
+                if (room && (room.getLastUsed() + this.coolDownTime < Date.now())) {
+                    this.io.to(roomId).emit(SocketEvent.PLAY);
+                    this.io.to(roomId).emit(SocketEvent.SYNCTIME, time);
+                    room.lastUsed = Date.now();
+                }
+            });
+
+            socket.on(SocketEvent.PAUSE, (roomId: string, time: number) => {
+                let room = this.currentRooms.get(roomId);
+                if (room && (room.getLastUsed() + this.coolDownTime < Date.now())) {
+                    this.io.to(roomId).emit(SocketEvent.PAUSE);
+                    this.io.to(roomId).emit(SocketEvent.SYNCTIME, time);
+                    room.lastUsed = Date.now();
+                }
+            });
+            socket.on(SocketEvent.NEXT, (roomId: string, nextVidId: string) => {
+                let room = this.currentRooms.get(roomId);
+                if (room && (room.getLastUsed() + this.coolDownTime < Date.now())) {
+                    this.io.to(roomId).emit(SocketEvent.SET_VID, nextVidId);
+                    this.io.to(roomId).emit(SocketEvent.ReadRoom, "Next!");
+                    let room = this.currentRooms.get(roomId);
+                    room.lastUsed = Date.now();
+                }
+            });
+            // Should not be called atm
+            socket.on(SocketEvent.SYNCTIME, (roomId: string, time: number) => {
+                let room = this.currentRooms.get(roomId);
+                this.io.to(roomId).emit(SocketEvent.SYNCTIME, time);
+                if (room) {
+                    room.lastUsed = Date.now();
+                }
+            });
+
+            socket.on(SocketEvent.ReadRoom, (roomId: string, cause: string) => {
+                let room = this.currentRooms.get(roomId);
+                if (room && (room.getLastUsed() + this.coolDownTime < Date.now())) {
+                    this.io.to(roomId).emit(SocketEvent.ReadRoom, cause);
+                    console.log(cause + " in " + roomId);
+                }
+            });
+
+            socket.on(SocketEvent.MSG, (roomId: string, msg: string, author: string) => {
+                if (!msg.replace(/\s/g, '').length) {
+                    socket.emit(SocketEvent.MSG, "Unable to Send (whitespace/empty msg)");
+                }
+                else {
+                    this.io.to(roomId).emit(SocketEvent.MSG, msg, author);
+                }
+            });
+
+            socket.on(SocketEvent.LOAD_VID, async (roomId: string, videoID: string) => {
+                if (this.ytApi.ready) {
+                    this.io.to(roomId).emit(SocketEvent.LOAD_VID, await this.ytApi.getVidInfo(videoID));
+                }
+            });
+
+            socket.on(SocketEvent.searchYT, async (searchTerm: string) => {
+                if (this.ytApi.ready) {
+                    socket.emit(SocketEvent.searchYT, await this.ytApi.searchKeyWord(searchTerm));
+                }
+            });
+
+            socket.on(SocketEvent.playlistVideos, async (searchTerm: string) => {
+                if (this.ytApi.ready) {
+                    socket.emit(SocketEvent.playlistVideos, await this.ytApi.getPlaylistVideos(searchTerm));
+                }
+            });
+
+            /* socket.on('setYTApi', (pw: string, apiKey: string) => {
+                if (mongooseDB.instance.checkPw(pw)) {
+                    this.ytApi.setApiKey(apiKey);
+                }
+            }); */
+        });
+
+    }
+}

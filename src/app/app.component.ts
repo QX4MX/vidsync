e871924468fa0c1cd99a769ef16fee815d96fa6f
qx4mx <qx4mx1@gmail.com>
2020-06-17 16:13:19 +0200

    load ytapi script in index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,7 @@ export class AppComponent {
     }
 
     ngOnInit(): void {
-        let tag = document.createElement('script');
-        tag.src = "https://www.youtube.com/iframe_api";
-        let firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
     }
 
 

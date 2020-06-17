f26e5461b067d594d7837f1d4f1a0bbc3b255713
qx4mx <qx4mx1@gmail.com>
2020-06-17 16:10:28 +0200

    added google login/ logout to sidenav

--- src/app/layout/sidenav/sidenav.component.ts
+++ src/app/layout/sidenav/sidenav.component.ts
@@ -1,14 +1,50 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, NgZone } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-	selector: 'app-sidenav',
-	templateUrl: './sidenav.component.html',
-	styleUrls: ['./sidenav.component.scss']
+    selector: 'app-sidenav',
+    templateUrl: './sidenav.component.html',
+    styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
-	constructor() { }
+    user;
+    constructor(private authService: AuthService, private ngZone: NgZone, private router: Router) {
+    }
 
-	ngOnInit(): void {
-	}
+    async ngOnInit(): Promise<void> {
+        if (await this.authService.checkIfUserAuthenticated()) {
+            this.user = this.authService.user;
+        }
+    }
 
+    async login(): Promise<gapi.auth2.GoogleUser> {
+        // Initialize gapi if not done yet
+        if (!this.authService.gapiSetup) {
+            await this.authService.initGoogleAuth();
+        }
+
+        // Resolve or reject signin Promise
+        return new Promise(async () => {
+            await this.authService.authInstance.signIn().then(
+
+                user => {
+                    this.user = user
+                    this.authService.user = user
+                },
+                error => this.authService.error = error);
+        });
+    }
+
+    logout() {
+        // Resolve or reject signin Promise
+        return new Promise(async () => {
+            await this.authService.authInstance.disconnect().then(
+                user => {
+                    this.authService.user = user
+                    this.user = this.authService.user
+                },
+                error => this.authService.error = error);
+        });
+    }
 }

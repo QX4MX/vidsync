c63972334a62091a5fb255081b1283ff0928851d
qx4mx <qx4mx1@gmail.com>
2020-06-21 14:38:30 +0200

    disable own login/ register

--- src/app/index/login/login.component.ts
+++ src/app/index/login/login.component.ts
@@ -41,7 +41,7 @@ export class LoginComponent implements OnInit {
 
     }
 
-    async onSubmit() {
+    /* async onSubmit() {
         // TODO
         this.loginInvalid = false;
         this.formSubmitAttempt = false;
@@ -56,7 +56,7 @@ export class LoginComponent implements OnInit {
         } else {
             this.formSubmitAttempt = true;
         }
-    }
+    } */
 
     async googleLogin() {
         //TODO redirect / sidenav update

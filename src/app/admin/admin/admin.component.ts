299a817fc59dde894b6f3f31f490adcd7e8ebffa
qx4mx <qx4mx1@gmail.com>
2020-07-03 17:26:47 +0200

    adjust admin to new auth

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { SocketService } from 'src/app/services/socket.service';
 import { Title } from '@angular/platform-browser';
-import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
     selector: 'app-admin',
@@ -13,7 +12,7 @@ export class AdminComponent implements OnInit {
     socket: SocketIOClient.Socket;;
     rooms: any = [];
     pw: string;
-    constructor(private apiService: ApiService, socketService: SocketService, private titleService: Title, private authService: AuthService) {
+    constructor(private apiService: ApiService, socketService: SocketService, private titleService: Title) {
         this.socket = socketService.socket;
         this.titleService.setTitle("Admin Page");
     }
@@ -21,27 +20,20 @@ export class AdminComponent implements OnInit {
     ngOnInit() { }
 
     async readRooms() {
-        if (await this.authService.checkIfUserAuthenticated()) {
-            let val = await this.apiService.adminGetRooms();
-            val.subscribe((data) => {
-                console.log(data);
-                let json = JSON.stringify(data);
-                var obj = JSON.parse(json);
-                this.rooms = obj.rooms;
+        if (this.apiService.token) {
+            (await this.apiService.adminGetRooms()).subscribe((res: any) => {
+                if (res.success) {
+                    this.rooms = res.data;
+                }
             });
         }
 
     }
 
-
     async removeRoom(room, index) {
-        if (await this.authService.checkIfUserAuthenticated() && window.confirm('Are you sure?')) {
-            let val = await this.apiService.adminDeleteRoom(room._id);
-            val.subscribe((data) => {
-                console.log(data);
-                let json = JSON.stringify(data);
-                var obj = JSON.parse(json);
-                if (obj.response == "Room deleted successfully") {
+        if (this.apiService.token && window.confirm('Are you sure?')) {
+            (await this.apiService.adminDeleteRoom(room._id)).subscribe((res) => {
+                if (res.success && res.response == "Room deleted successfully") {
                     this.rooms.splice(index, 1);
                 }
             });

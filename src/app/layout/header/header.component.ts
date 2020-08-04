9c36a341051ee12e9a1e49e341fadb7ab0162a05
qx4mx <qx4mx1@gmail.com>
2020-08-04 17:07:19 +0200

    added gitlab/ discord to header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Output, EventEmitter, HostListener } from '@angular/core';
 import { MatBottomSheet, MatBottomSheetRef } from '@angular/material/bottom-sheet';
-import { SettingsComponent } from 'src/app/main/settings/settings.component';
 import { UserinfoComponent } from 'src/app/main/userinfo/userinfo.component';
 
 @Component({
@@ -25,10 +24,6 @@ export class HeaderComponent implements OnInit {
         else {
             this.smallScreen = false;
         }
-    }
-
-    openSettings(): void {
-        this.settings.open(SettingsComponent);
     }
 
     openUserInfo(): void {
